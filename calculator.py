575195db1134cd66769f6b88c0327e23a42c93bd
narattscoward <samyati.syt@gmail.com>
2026-02-23 00:45:51 +0700

    Test quality gate

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
 def add(a, b):
     return a + b
 
-
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
 
 
-# def demo_fail():
-#     unused_variable = 123   # Sonar will detect unused variable
-#     return 1
+# def old():
+#     print("This is an old function that should be removed")
+
+def fail():
+    x = 1
+    return 5 
+  
