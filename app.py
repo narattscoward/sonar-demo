950c00a17112114dff25c041176e9c1ee4c21ac6
narattscoward <samyati.syt@gmail.com>
2026-02-22 14:26:09 +0700

    Initial commit - bad quality version

---
+++ app.py
@@ -0,0 +1,6 @@
+from calculator import add, divide
+
+password = "123456"  # hardcoded password (security issue)
+
+print(add(10, 5))
+print(divide(10, 0))
---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    if b == 0:
+        print("Cannot divide by zero")  # bad practice
+    return a / b
+
+def duplicate_code():
+    x = 5
+    y = 10
+    return x + y
+
+def duplicate_code2():
+    x = 5
+    y = 10
+    return x + y
